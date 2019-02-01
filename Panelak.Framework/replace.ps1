7450d4b3cf7c064d7e1766ca86cc4fff162d53d2
Miroslav Bartl <18615545+czmotig@users.noreply.github.com>
2019-02-01 13:02:16 +0100

    cleanup after rename, added icon

--- Panelak.Framework/replace.ps1
+++
@@ -1,8 +0,0 @@
-$s = "Panelak"
-$r = "Panelak"
-
-Get-ChildItem "C:\Users\miros\source\repos\Panelak\Panelak.Framework" -Recurse -Filter *.* | % {
-  (Get-Content $_.FullName) `
-    | % { $_ -replace [regex]::Escape($s), $r } `
-    | Set-Content $_.FullName
-}
